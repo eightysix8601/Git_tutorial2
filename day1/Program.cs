79d0e739b41d25bb599e0364f9d8d758252c8977
eightysix8601@gmail.com <eightysix8601>
2023-07-13 00:45:39 +0900

    Initial Commit

---
+++ day1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int n = 10;
+int n2;
+n2 = 100;
+
+long sum = n + n2;
+
+float sum1 = 1.12f;
+double sum2 = 1.234 * 2.3213;
+decimal sum3 = 123.321312312321m * 7.213321321321321312312m;
+
+Console.WriteLine(sum);
+Console.WriteLine(sum1);
+Console.WriteLine(sum2);
+Console.WriteLine(sum3);
